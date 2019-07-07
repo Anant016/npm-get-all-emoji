216ba5de6ce0044c2696101f78dbb30d5ae43115
Anant Rungta <anantrungta1999@gmail.com>
2019-07-07 13:11:21 +0530

    updated

---
+++ Readme.md
@@ -0,0 +1,39 @@
+[![npm version](https://badge.fury.io/js/node.svg)](https://badge.fury.io/js/node)
+
+## Installation
+
+This is a [Node.js](https://nodejs.org/en/) module available through the
+[npm registry](https://www.npmjs.com/).
+
+Before installing, [download and install Node.js](https://nodejs.org/en/download/).
+Node.js 0.10 or higher is required.
+
+Run `npm i -S get-all-emoji`
+
+### Example
+
+```javascript
+const getEmoji = require("get-all-emoji");
+console.log(getEmoji("happy"));
+
+or;
+
+var q = getEmoji("family_one_girl");
+```
+
+## Documentation
+
+Simply take the name of any emoji from more than **1800** emojis present, and pass it as a parametere as shown in above example.
+
+You'll find the _name of emoji_ from the links present below, which are encoded, and you can replace it instead of happy in above diagram.
+
+## keywords encoded
+
+1. [w3-emojis](https://www.w3schools.com/charsets/ref_emoji_smileys.asp)
+2. [special-emojis](https://github.com/Anant016/get-all-emoji/blob/master/list.js)
+
+## Support
+
+If you want to support, please consider donating. It'll help me in development and making more cool packages for you:
+
+[![Donate](./paypaldonate.webp)](https://paypa.me/ARungta)
---
+++ index.js
@@ -0,0 +1,88 @@
+//const request = require("request");
+//const cheerio = require("cheerio");
+const list = require("./list");
+
+function getEmoji(emojiname) {
+  var emoji = "&#";
+  var emojiname = emojiname.toUpperCase();
+  switch (emojiname) {
+    case "GRINNING FACE":
+      emoji += "128512";
+      break;
+
+    case "HAPPY":
+      emoji += "128512";
+      break;
+
+    case "SAD":
+      emoji += "128577";
+      break;
+
+    case "RUDE":
+      emoji += "128547";
+      break;
+
+    case "ANGRY":
+      emoji += "128548";
+      break;
+
+    case "CRY":
+      emoji += "129319";
+      break;
+
+    case "GRINNING FACE WITH SMILING EYES":
+      emoji += "128513";
+      break;
+
+    case "FACE WITH TEARS OF JOY":
+      emoji += "128514";
+      break;
+
+    case "SMILING FACE WITH OPEN MOUTH":
+      emoji += "128515";
+      break;
+
+    case "SMILING FACE WITH OPEN MOUTH AND SMILING EYES":
+      emoji += "128516";
+      break;
+
+    case "SMILING FACE WITH OPEN MOUTH AND COLD SWEAT":
+      emoji += "128517";
+      break;
+
+    case "SMILING FACE WITH OPEN MOUTH AND TIGHTLY-CLOSED EYES":
+      emoji += "128518";
+      break;
+
+    case "SMILING FACE WITH HALO":
+      emoji += "128519";
+      break;
+
+    case "SMILING FACE WITH HORNS":
+      emoji += "128520";
+      break;
+
+    case "WINKING FACE":
+      emoji += "128521";
+      break;
+
+    case "SMILING FACE WITH SMILING EYES":
+      emoji += "128522";
+      break;
+
+    case list.emojis[0].name.toUpperCase():
+      emoji += list.emojis[0].html;
+      break;
+    default:
+      list.emojis.map(function(item) {
+        if (emojiname == item.name.toUpperCase()) {
+          emoji = item.html;
+        }
+      });
+      break;
+  }
+  return emoji;
+}
+console.log(getEmoji("family_one_girl"));
+
+module.exports = getEmoji;
